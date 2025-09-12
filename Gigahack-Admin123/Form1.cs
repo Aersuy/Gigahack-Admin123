cad9c684ca9f17c10f528d5380672ecbcfb2a97b
Raileanu Vlad <raileanu1100@gmail.com>
2025-09-12 18:54:29 +0300

    Add project files.

---
+++ Gigahack-Admin123/Form1.cs
@@ -0,0 +1,10 @@
+namespace Gigahack_Admin123
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Gigahack-Admin123/Program.cs
@@ -0,0 +1,14 @@
+namespace Gigahack_Admin123
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+
+        }
+    }
+}
